c7004d9e596a0f0a0897500146b075a3eb913bb5
PavelKuzia <pavelkuznec132@gmail.com>
2025-10-03 20:08:25 +0200

    test docker compose on AWS EC2: v5

--- docker-commands-prod.bat
+++ docker-commands-prod.bat
@@ -2,11 +2,15 @@ echo "--- Starting Docker Command Sequence ---"
 
 docker compose -f docker-compose-prod.yml down 
 
+@REM docker compose -f docker-compose-prod.yml build --no-cache
+
 set ENV_AWS_RDS_ENDPOINT=edu-aws-db.cbu6mq86qkdr.eu-north-1.rds.amazonaws.com
 set ENV_USERNAME_RDS=postgres
 set ENV_PASSWORD_RDS=FmC0cThSogj0n23l8tJE
 set ENV_FRONTEND_PORT=4200
 set ENV_FRONTEND_URL=frontend
+
+set API_URL=backend
 
 docker compose -f docker-compose-prod.yml up -d
 
