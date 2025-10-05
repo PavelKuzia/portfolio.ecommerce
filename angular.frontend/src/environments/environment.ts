7e0719a737ebde897ad6254c91febd1fbc895711
PavelKuzia <pavelkuznec132@gmail.com>
2025-10-05 13:34:37 +0200

    test docker compose on AWS EC2: v17

--- angular.frontend/src/environments/environment.ts
+++ angular.frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://ec2-16-171-24-210.eu-north-1.compute.amazonaws.com:8005',
+  // API_URL: 'http://ec2-16-171-24-210.eu-north-1.compute.amazonaws.com:8005',
+  API_URL: '',
 };
